338d79bbdb225ca381527414d704f27f161df611
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-03-25 22:13:30 -0300

    Primeiro commit adicionado cod ao enum

--- src/br/compilador/com/TokenType.java
+++ src/br/compilador/com/TokenType.java
@@ -1,7 +1,50 @@
 package br.compilador.com;
 
 public enum TokenType {
-	EOF, NUM_INT, NUM_FLOAT, LITERAL, ID, RELOP, ARIT_AS, ARIT_MD, ASSIGN, TERM, L_PAR, R_PAR, LOGIC_VAL, LOGIC_OP,
-	TYPE, PROGRAM, END_PROG, BEGIN, END, IF, THEN, ELSE, FOR, WHILE, DECLARE, TO
+	EOF(1), 
+	NUM_INT(2), 
+	NUM_FLOAT(3), 
+	LITERAL(4), 
+	ID(5), 
+	RELOP(6), 
+	ARIT_AS(7), 
+	ARIT_MD(8), 
+	ASSIGN(9), 
+	TERM(10), 
+	L_PAR(11), 
+	R_PAR(12), 
+	LOGIC_VAL(13), 
+	LOGIC_OP(14),
+	TYPE(15), 
+	PROGRAM(16), 
+	END_PROG(17), 
+	BEGIN(18), 
+	END(19), 
+	IF(20), 
+	THEN(21), 
+	ELSE(22), 
+	FOR(23), 
+	WHILE(24), 
+	DECLARE(25), 
+	TO(26);
+	
+	private int cod;
+
+	private TokenType(int cod){
+		this.cod = cod;
+	}
+
+	public int getCod(){
+		return cod;
+	}
+	
+	public static TokenType toEnum(int cod){
+		
+		for (TokenType tokenType : TokenType.values()){
+			if(cod == tokenType.getCod()) { return tokenType; }
+		}
+		
+		throw new IllegalArgumentException("codigo invalido "+cod);
+	}
 
 }
