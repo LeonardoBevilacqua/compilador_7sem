2fac740111c06bb86cdf2178e958f77f99a417d6
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-03-27 17:18:25 -0300

    Adicionado comentario na classe TokenType

--- src/br/compilador/com/TokenType.java
+++ src/br/compilador/com/TokenType.java
@@ -1,32 +1,32 @@
 package br.compilador.com;
 
 public enum TokenType {
-	EOF(1), 
-	NUM_INT(2), 
-	NUM_FLOAT(3), 
-	LITERAL(4), 
-	ID(5), 
-	RELOP(6), 
-	ARIT_AS(7), 
-	ARIT_MD(8), 
-	ASSIGN(9), 
-	TERM(10), 
-	L_PAR(11), 
-	R_PAR(12), 
-	LOGIC_VAL(13), 
-	LOGIC_OP(14),
-	TYPE(15), 
-	PROGRAM(16), 
-	END_PROG(17), 
-	BEGIN(18), 
-	END(19), 
-	IF(20), 
-	THEN(21), 
-	ELSE(22), 
-	FOR(23), 
-	WHILE(24), 
-	DECLARE(25), 
-	TO(26);
+	EOF(0), 		//
+	NUM_INT(1), 	// 123,48, 3E+10
+	NUM_FLOAT(2), 	// 4.8, 3.10E+10
+	LITERAL(3), 	// “Carlos”, “Batata”
+	ID(4), 			// val, _salario, i__
+	RELOP(5), 		// $df, $gt, $le
+	ARIT_AS(6), 	// +, -
+	ARIT_MD(7), 	// *, /
+	ASSIGN(8), 		// <-
+	TERM(9), 		// ;
+	L_PAR(10), 		// (
+	R_PAR(11), 		// )
+	LOGIC_VAL(12), 	// true, false
+	LOGIC_OP(13), 	// and, not, or
+	TYPE(14), 		// bool, text, int
+	PROGRAM(15), 	// program
+	END_PROG(16), 	// end_prog
+	BEGIN(17), 		// begin
+	END(18), 		// end
+	IF(19), 		// if
+	THEN(20), 		// then
+	ELSE(21), 		// else
+	FOR(22), 		// for
+	WHILE(23), 		// while
+	DECLARE(24), 	// declare 
+	TO(25); 		// to
 	
 	private int cod;
 
