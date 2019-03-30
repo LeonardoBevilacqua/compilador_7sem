0d8aad5e3a1c7fe4f0c4e023b351b562ae73d4cf
Thiago Luiz <thiluiz.oliveira@gmail.com>
2019-03-30 20:44:31 -0300

    Adicionando palavras reservadas na tabela de simbolos

--- src/br/compilador/com/TabSimbolos.java
+++ src/br/compilador/com/TabSimbolos.java
@@ -10,6 +10,51 @@ public class TabSimbolos {
 	
 	private TabSimbolos() {
 		tab = new HashMap<String, Token>();
+
+		Token tokenTrue = new Token(TokenType.LOGIC_VAL, "true");
+		Token tokenFalse = new Token(TokenType.LOGIC_VAL, "false");
+		Token tokenNot = new Token(TokenType.LOGIC_OP, "not");
+		Token tokenAnd = new Token(TokenType.LOGIC_OP, "and");
+		Token tokenOr = new Token(TokenType.LOGIC_OP, "or");
+		Token tokenBool = new Token(TokenType.TYPE, "bool");
+		Token tokenText = new Token(TokenType.TYPE, "text");
+		Token tokenInt = new Token(TokenType.TYPE, "int");
+		Token tokenFloat = new Token(TokenType.TYPE, "float");
+		Token tokenProgram = new Token(TokenType.PROGRAM, "program");
+		Token tokenEndProgram = new Token(TokenType.END_PROG, "end_prog");
+		Token tokenBegin = new Token(TokenType.BEGIN, "begin");
+		Token tokenEnd = new Token(TokenType.END, "end");
+		Token tokenIf = new Token(TokenType.IF, "if");
+		Token tokenThen = new Token(TokenType.THEN, "then");
+		Token tokenElse = new Token(TokenType.ELSE, "else");
+		Token tokenFor = new Token(TokenType.FOR, "for");
+		Token tokenWhile = new Token(TokenType.WHILE, "while");
+		Token tokenDeclare = new Token(TokenType.DECLARE, "declare");
+		Token tokenTo = new Token(TokenType.TO, "to");
+
+
+		tab.put(tokenTrue.getLexema(), tokenTrue);
+		tab.put(tokenFalse.getLexema(), tokenFalse);
+		tab.put(tokenNot.getLexema(), tokenNot);
+		tab.put(tokenAnd.getLexema(), tokenAnd);
+		tab.put(tokenOr.getLexema(), tokenOr);
+		tab.put(tokenBool.getLexema(), tokenBool);
+		tab.put(tokenText.getLexema(), tokenText);
+		tab.put(tokenInt.getLexema(), tokenInt);
+		tab.put(tokenFloat.getLexema(), tokenFloat);
+		tab.put(tokenProgram.getLexema(), tokenProgram);
+		tab.put(tokenEndProgram.getLexema(), tokenEndProgram);
+		tab.put(tokenBegin.getLexema(), tokenBegin);
+		tab.put(tokenEnd.getLexema(), tokenEnd);
+		tab.put(tokenIf.getLexema(), tokenIf);
+		tab.put(tokenThen.getLexema(), tokenThen);
+		tab.put(tokenElse.getLexema(), tokenElse);
+		tab.put(tokenFor.getLexema(), tokenFor);
+		tab.put(tokenWhile.getLexema(), tokenWhile);
+		tab.put(tokenDeclare.getLexema(), tokenDeclare);
+		tab.put(tokenTo.getLexema(), tokenTo);
+
+
 		
 	}
 	
--- src/br/compilador/com/Token.java
+++ src/br/compilador/com/Token.java
@@ -4,16 +4,23 @@ package br.compilador.com;
 public class Token {
 	private TokenType tokenType; // tipo de token 
 	private String lexema; // cadeia de caracteres do token
-	private int lin; // linha em que o token ocorre
-	private int col; // coluna do 1o caractere do token
+	private int linha; // linha em que o token ocorre
+	private int coluna; // coluna do 1o caractere do token
 
-	
+
+
+	public Token(TokenType tokenType, String lexema) {
+		super();
+		this.tokenType = tokenType;
+		this.lexema = lexema;
+	}
+
 	public Token(TokenType tokenType, String lexema, int lin, int col) {
 		super();
 		this.tokenType = tokenType;
 		this.lexema = lexema;
-		this.lin = lin;
-		this.col = col;
+		this.linha = linha;
+		this.coluna = coluna;
 	}
 
 	public void print() {
@@ -22,11 +29,11 @@ public class Token {
 	}
 
 	public TokenType getTokenType() {
-		return tokenType;
+		return this.tokenType;
 	}
 
 	public String getLexema() {
-		return lexema;
+		return this.lexema;
 	}
 
 	
