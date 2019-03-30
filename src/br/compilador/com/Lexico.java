66b7485ae7af16cbf8bc0a9fb03cbe0bbf0dc615
LEONARDO ALMEIDA BEVILACQUA <1510033187@acad.dvuadmin.net>
2019-03-29 21:26:52 -0300

    Ajuste no Lexico.java

--- src/br/compilador/com/Lexico.java
+++ src/br/compilador/com/Lexico.java
@@ -27,10 +27,35 @@ public class Lexico {
 				switch (state) {
 				case 0:
 					c = fl.getNextChar();
+					if(c == '<') {
+						state = 1;
+					}else if(c == '=') {
+						state = 5;
+					}else {
+						state = 6;
+					}
 					break;
 				// ...
 				case 9:
-					
+					c = fl.getNextChar();
+					if(Character.isLetter(c)) {
+						state = 10;
+					}else {
+						state = falhar(9);
+					}
+					break;
+				case 10:
+					c = fl.getNextChar();
+					if(Character.isLetter(c) || Character.isDigit(c)) {
+						state = 10;
+					}else {
+						state = 11;
+					}
+					break;
+				case 11:
+					retrair(1);	
+					instalarId();
+					return obter_token();
 				default:
 					break;
 				}
@@ -41,6 +66,40 @@ public class Lexico {
 		
 		
 		return null;
+	}
+
+	private Token obter_token() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void instalarId() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void retrair(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Em caso de falha, deve retornar o estado para iniciar a analise da proxima maquina
+	 * @param partida
+	 * @return O estado
+	 */
+	private int falhar(int partida) { 
+		long pt_adiante = fl.getColumn();
+		switch (partida) {
+		case 12:
+			partida = 20; 
+			break;
+
+		default:
+			// erro lexico
+			break;
+		}
+		return partida;
 	}
 
 }
