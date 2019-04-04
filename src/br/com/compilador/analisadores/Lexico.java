d5ec2fd45ac53defeb8205f00b8298512694ce02
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-04-03 21:03:09 -0300

    Refatoração do lexico, criação da verificação numerica

--- src/br/com/compilador/analisadores/Lexico.java
+++ src/br/com/compilador/analisadores/Lexico.java
@@ -1,5 +1,6 @@
 package br.com.compilador.analisadores;
 
+import java.io.EOFException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -9,130 +10,111 @@ import br.com.compilador.utils.ErrorHandler;
 import br.com.compilador.utils.FileLoader;
 
 public class Lexico {
-	
+
 	private FileLoader fl;
 	private ErrorHandler errorH;
 	private String lexema = "";
 	private TokenType tokenType = null;
-	
+
 	public Lexico(String filename) {
 		errorH = ErrorHandler.getInstance();
 		try {
 			fl = new FileLoader(filename);
 		} catch (FileNotFoundException e) {
 			errorH.registraErro(e.getMessage());
-		} 
+		}
 	}
-	
+
 	public Token nextToken() {
-		char c = ' ';		
-		int state = 0;
-		
+		char c = ' ';
+
 		lexema = "";
 		tokenType = null;
-		
+
 		try {
-			// elimina brancos
-			while(true) {
+			do {
 				c = fl.getNextChar();
-				if(Character.isWhitespace(c)) {
-					continue;
-				}
-				
-				if(verifica_caracteres_simples(c)) {
-					return obter_token();
-				}				
-	
-				
-				switch (state) {
-				case 0:
-					if(c == '<') {
-						state = 1;
-					}else if(c == '=') {
-						state = 5;
-					}else {
-						state = 6;
-					}
-					break;
-				// ...
-				case 9:
-					c = fl.getNextChar();
-					if(Character.isLetter(c)) {
-						state = 10;
-					}else {
-						state = falhar(9);
-					}
-					break;
-				case 10:
-					c = fl.getNextChar();
-					if(Character.isLetter(c) || Character.isDigit(c)) {
-						state = 10;
-					}else {
-						state = 11;
-					}
-					break;
-				case 11:
-					retrair(1);	
-					instalarId();
-					return obter_token();
-				default:
-					break;
-				}
-			}
-		} catch (IOException e) {
-			//errorH.registraErro(e.getMessage());
-			return new Token(TokenType.EOF, lexema, fl.getLine(), fl.getColumn());
+			} while (Character.isWhitespace(c));			
+		} catch (IOException e) {			
+			return new Token(TokenType.EOF, "", fl.getLine(), fl.getColumn());
 		}
+		
+		if (Character.isDigit(c)) {
+			verifica_float_int(c);
+		}else if (verifica_caracteres_simples(c)) {
+			return obter_token();
+		}
+		
+		return null;
 	}
-	
-	private boolean verifica_caracteres_simples(char c) {	
-		if(c == '+' || c == '-') {
+
+	private boolean verifica_caracteres_simples(char c) {
+		if (c == '+' || c == '-') {
 			tokenType = TokenType.ARIT_AS;
-		}else if(c == '*' || c == '/') {
+		} else if (c == '*' || c == '/') {
 			tokenType = TokenType.ARIT_MD;
-		}else if(c == ';') {
+		} else if (c == ';') {
 			tokenType = TokenType.TERM;
-		}else if(c == '(') {
+		} else if (c == '(') {
 			tokenType = TokenType.L_PAR;
-		}else if(c == ')') {
-			tokenType = TokenType.R_PAR;		
-		}else {
+		} else if (c == ')') {
+			tokenType = TokenType.R_PAR;
+		} else {
 			return false;
 		}
-		
+
 		lexema += c;
 		return true;
 	}
-	
-	private void verifica_relop() {
-		
+
+	private boolean verifica_float_int(char c) {
+		if (!Character.isDigit(c) && c != 'E' && c != '.') {
+			return false;
+		}
+
+		if (tokenType == null) {
+			tokenType = TokenType.NUM_INT;
+		}
+
+		if (c == '.') {
+			tokenType = TokenType.NUM_FLOAT;
+		}
+		lexema += c;
+
+		return true;
 	}
 
+	private void verifica_relop() {
+
+	}
 
 	private Token obter_token() {
+		// reavaliar linha e coluna
 		return new Token(tokenType, lexema, fl.getLine(), fl.getColumn());
 	}
 
 	private void instalarId() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void retrair(int i) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
-	 * Em caso de falha, deve retornar o estado para iniciar a analise da proxima maquina
+	 * Em caso de falha, deve retornar o estado para iniciar a analise da proxima
+	 * maquina
+	 * 
 	 * @param partida
 	 * @return O estado
 	 */
-	private int falhar(int partida) { 
+	private int falhar(int partida) {
 		long pt_adiante = fl.getColumn();
 		switch (partida) {
 		case 12:
-			partida = 20; 
+			partida = 20;
 			break;
 
 		default:
