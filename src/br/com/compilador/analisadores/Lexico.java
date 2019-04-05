3f35a4528019cbec1e2c9655e7da6d220ae3867c
LeonardoBevilacqua <leonardo_bevilacqua@hotmail.com>
2019-04-05 19:41:42 -0300

    adapdatado para stringBuilder, removido funções em desuso

--- src/br/com/compilador/analisadores/Lexico.java
+++ src/br/com/compilador/analisadores/Lexico.java
@@ -4,7 +4,6 @@ import java.io.EOFException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import br.com.compilador.TabSimbolos;
 import br.com.compilador.token.Token;
 import br.com.compilador.token.TokenType;
 import br.com.compilador.utils.ErrorHandler;
@@ -38,7 +37,7 @@ public class Lexico {
 		try {
 			do {
 				c = fl.getNextChar();
-				if (lexema == "") {
+				if (lexema.length() == 0) {
 					coluna_inicial = fl.getColumn();
 				}
 
@@ -47,7 +46,7 @@ public class Lexico {
 						break;
 					}
 				} else if (c == '$') {
-					if(verificarRelop()) {
+					if (verificarRelop()) {
 						break;
 					}
 				} else if (verifica_caracteres_simples()) {
@@ -56,7 +55,7 @@ public class Lexico {
 
 			} while (Character.isWhitespace(c));
 		} catch (IOException e) {
-			if (lexema != "") {
+			if (lexema.length() != 0) {
 				try {
 					fl.resetLastChar();
 				} catch (IOException e1) {
@@ -81,8 +80,7 @@ public class Lexico {
 		tokenType = TokenType.NUM_INT;
 
 		do {
-			lexema += c;
-			c = fl.getNextChar();
+			next_character();
 		} while (Character.isDigit(c));
 
 		if (c == '.') {
@@ -101,11 +99,15 @@ public class Lexico {
 		return true;
 	}
 
+	private void next_character() throws EOFException, IOException {
+		lexema.append(c);
+		c = fl.getNextChar();
+	}
+
 	private boolean verifica_float() throws EOFException, IOException {
 		tokenType = TokenType.NUM_FLOAT;
 		do {
-			lexema += c;
-			c = fl.getNextChar();
+			next_character();
 		} while (Character.isDigit(c));
 
 		if (c == 'E' || c == 'e') {
@@ -119,8 +121,7 @@ public class Lexico {
 
 	private boolean verifica_notacao() throws EOFException, IOException {
 		do {
-			lexema += c;
-			c = fl.getNextChar();
+			next_character();
 		} while (Character.isDigit(c) || (c == '+' || c == '-'));
 		if (!Character.isWhitespace(c)) {
 			return false;
@@ -144,90 +145,54 @@ public class Lexico {
 			return false;
 		}
 
-		lexema += c;
+		lexema.append(c);
 		return true;
 	}
 
 	private Token obter_token() {
-		// reavaliar linha e coluna
-		return new Token(tokenType, lexema, fl.getLine(), coluna_inicial);
-	}
-
-	private void instalarId() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void retrair(int i) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * Em caso de falha, deve retornar o estado para iniciar a analise da proxima
-	 * maquina
-	 * 
-	 * @param partida
-	 * @return O estado
-	 */
-	private int falhar(int partida) {
-		long pt_adiante = fl.getColumn();
-		switch (partida) {
-		case 12:
-			partida = 20;
-			break;
-
-		default:
-			// erro lexico
-			break;
-		}
-		return partida;
+		return new Token(tokenType, lexema.toString(), fl.getLine(), coluna_inicial);
 	}
 
 	private boolean verificarRelop() throws EOFException, IOException {
-		lexema += c;
-		c = fl.getNextChar();
+		next_character();
 		if (c == 'l' | c == 'g') {
-			lexema += c;
-			c = fl.getNextChar();
+			next_character();
 			if (c == 't' | c == 'e') {
-				lexema += c;
+				lexema.append(c);
 				tokenType = TokenType.RELOP;
 			} else {
-				lexema += c;
+				lexema.append(c);
 				errorH.registraErro("lexema errado :" + lexema);
-				lexema = "";
+				lexema.setLength(0);
 				return false;
 			}
 		} else if (c == 'e') {
-			lexema += c;
-			c = fl.getNextChar();
+			next_character();
 			if (c == 'q') {
-				lexema += c;
+				lexema.append(c);
 				tokenType = TokenType.RELOP;
 			} else {
-				lexema += c;
+				lexema.append(c);
 				errorH.registraErro("lexema errado :" + lexema);
-				lexema = "";
+				lexema.setLength(0);
 				return false;
 			}
 		} else if (c == 'd') {
-			lexema += c;
-			c = fl.getNextChar();
+			next_character();
 			if (c == 'f') {
-				lexema += c;
+				lexema.append(c);
 				tokenType = TokenType.RELOP;
 
 			} else {
-				lexema += c;
+				lexema.append(c);
 				errorH.registraErro("lexema errado :" + lexema);
-				lexema = "";
+				lexema.setLength(0);
 				return false;
 			}
 		} else {
-			lexema += c;
+			lexema.append(c);
 			errorH.registraErro("lexema errado :" + lexema);
-			lexema = "";
+			lexema.setLength(0);
 			fl.resetLastChar();
 			return false;
 		}
