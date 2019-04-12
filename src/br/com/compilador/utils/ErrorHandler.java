8fbd9c46a0e979f4f608fd0c81a3b893756949c3
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2019-04-12 18:33:43 -0300

    Update ErrorHandler.java

--- src/br/com/compilador/utils/ErrorHandler.java
+++ src/br/com/compilador/utils/ErrorHandler.java
@@ -1,3 +1,9 @@
+/**
+* Leonardo Almeida Bevilacqua 1510033187
+* Marcelo Costa 1510030281
+* Marcio Macedo 1510029701
+* Thiago Oliveira 1510028818
+*/
 package br.com.compilador.utils;
 
 import java.util.ArrayList;
@@ -8,6 +14,9 @@ public class ErrorHandler {
 	private static ErrorHandler instance = new ErrorHandler();
 	private List<String> errosLexicos = new ArrayList<String>();
 	
+	/**
+	* Metodo responsavel por registrar erros lexicos
+	*/
 	public void registrarErroLexico(ErrorType errorType, String lexema, long linha, long coluna)
 	{
 		String erroLexico = "Lexema errado: "+lexema+" Tipo do Erro: "+errorType+" Linha: "+linha+" Coluna: "+coluna;
@@ -24,13 +33,19 @@ public class ErrorHandler {
 		}
 	}
 	
+	/**
+	* Metodo responsavel por imprimir o cabeçalho de erros
+	*/
 	private void cabecalhoRelatorioLexico()
 	{
 		System.out.println("\n---------------------------------------");
 		System.out.printf("|%25s %12s","Erros","|");
 		System.out.println("\n---------------------------------------");
 	}
-
+	
+	/**
+	* Metodo responsavel por retornar a instancia unica da classe
+	*/
 	public static ErrorHandler getInstance() 
 	{
 		return instance;
