58860561e7d7cb5694d44fa534b9750c02d65396
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2019-04-12 18:32:25 -0300

    Update ArquivoNaoEncontradoException.java

--- src/br/com/compilador/utils/ArquivoNaoEncontradoException.java
+++ src/br/com/compilador/utils/ArquivoNaoEncontradoException.java
@@ -1,5 +1,14 @@
+/**
+* Leonardo Almeida Bevilacqua 1510033187
+* Marcelo Costa 1510030281
+* Marcio Macedo 1510029701
+* Thiago Oliveira 1510028818
+*/
 package br.com.compilador.utils;
 
+/**
+* Responsavel por exibir uma mensagem em caso de erro de leitura do arquivo
+*/
 public class ArquivoNaoEncontradoException extends Exception
 {
 	private static final long serialVersionUID = 1L;
