af011a94a9b47d6de25a0f570386447cea3e23f3
Marcelo Rodrigues <capp59marcelo@hotmail.com>
2019-05-25 17:05:37 -0300

    Criado interface dos metodos que deverá ter na classe Sintatico

---
+++ src/br/com/compilador/analisadores/Isintatico.java
@@ -0,0 +1,36 @@
+package br.com.compilador.analisadores;
+
+public interface Isintatico
+{
+	void derivaS();
+	void derivaCmds();
+	void derivaCmd();
+	void derivaDecl();
+	void derivaCond();
+	void derivaCndb();
+	void derivaAtrib();
+	void derivaExp();
+	void derivaFID();
+	void derivaFOPNum();
+	void derivaFEXPNum_1();
+	void derivaFNUMInt();
+	void derivaFOPNum1();
+	void derivaFEXPNum_2();
+	void derivaFNumFloat();
+	void derivaFOPNum2();
+	void derivaFEXPNum_3();
+	void derivaFLPar();
+	void derivaFEXPNum();
+	void derivaFRPar();
+	void derivaFID_1();
+	void derivaFValLog();
+	void derivaXEXPNum();
+	void derivaOPnum();
+	void derivaVal();
+	void derivaRep();
+	void derivaRepf();
+	void derivaEXPNum();
+	void derivaREPW();
+	void derivaExplo();
+	void derivaBloco();
+}
